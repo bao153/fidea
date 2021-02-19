fde5e71f8f6d4330c7c290fdcd4388e8adbfef17
Bao Nguyen <hoaibaonguyen1999@gmail.com>
2021-02-18 18:41:26 -0800

    fix page length and navbar

--- src/components/lib/CustomNavbar/CustomNavbar.js
+++ src/components/lib/CustomNavbar/CustomNavbar.js
@@ -12,18 +12,25 @@ const CustomNavbar = (props) => {
   }
 
   return (
-      <Navbar style={{height: "7rem"}} bg="dark" variant="dark" fixed="bottom">
-        <Nav onSelect={handleSelect} 
-          className="justify-content-around" 
-          style={{width: "100rem"}} 
-          defaultActiveKey="/"
-          activeKey={activeKey}
-        >
-          <Nav.Link as={NavLink} exact to="/" eventKey="home">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/ingredients" eventKey="ingredients">Ingredients</Nav.Link>
-          <Nav.Link as={NavLink} to="/saved" eventKey="saved">Saved</Nav.Link>
-          <Nav.Link as={NavLink} to="/profile" eventKey="profile">Profile</Nav.Link>
-        </Nav>
+      <Navbar 
+        collapseOnSelect
+        bg="dark" variant="dark" 
+        fixed="bottom"
+        expand="sm"
+      >
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-left">
+          <Nav onSelect={handleSelect} 
+            className="justify-content-around" 
+            defaultActiveKey="/"
+            activeKey={activeKey}
+          >
+            <Nav.Link as={NavLink} exact to="/" eventKey="home">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/ingredients" eventKey="ingredients">Ingredients</Nav.Link>
+            <Nav.Link as={NavLink} to="/saved" eventKey="saved">Saved</Nav.Link>
+            <Nav.Link as={NavLink} to="/profile" eventKey="profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
   )
 }
